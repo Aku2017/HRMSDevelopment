f47f2d4e24cf7ca0905a912eb8a46fdc250c2bae
Michael <akumichael2011@gmail.com>
2023-01-20 11:06:52 +0100

    push recent updates

---
+++ HRMScore/Entities/MaritalInformation.cs
@@ -0,0 +1,20 @@
+﻿using HRMScore.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRMS.Domain.Entities
+{
+    public class MaritalInformation: BaseEntity
+    {
+        public string? SpouseName { get; set; }
+        public string? SpousePhoneNumber { get; set; }
+        public string? SpouseProfession { get; set; }
+        public string? ResidentialAddress { get; set; }
+        public int NumberOfChildren { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+    }
+}
